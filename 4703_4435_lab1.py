de614169bb8e69d15a9364ebe666c69d93fe6a05
Hagar Barakat <hagarbarakat@hotmail.com>
2020-03-20 03:56:47 +0200

    Update 4703_4435_lab1.py

--- 4703_4435_lab1.py
+++ 4703_4435_lab1.py
@@ -48,6 +48,7 @@ class TftpProcessor(object):
         self.blocknumber = 1
         self.last = -1
         self.packet_buffer = []
+        self.file = []
         pass
 
     def process_udp_packet(self, packet_data, packet_source):
@@ -63,9 +64,10 @@ class TftpProcessor(object):
         print(f"Received a packet from {packet_source}")
         in_packet = self._parse_udp_packet(packet_data)
         out_packet = self._do_some_logic(in_packet)
-
         # This shouldn't change.
-        self.packet_buffer.append(out_packet)
+        if out_packet != -1:
+            self.packet_buffer.append(out_packet)
+        return in_packet
 
     def _parse_udp_packet(self, packet_byte):
         """
@@ -73,16 +75,34 @@ class TftpProcessor(object):
         the type of the packet and extract other available
         information.
         """
-        # pull // pack -> data, wrq // unpack -> acq
-
-        # PACK OR UNPACK
-        pass
+        opcode = packet_byte[:2]
+        unpacking = unpack("!h", opcode)
+        if unpacking[0] == 3:
+            return self.data(packet_byte), 0
+        if unpacking[0] == 4:
+            return self.ack(packet_byte), 1
+        else:
+            return self.error(packet_byte), 2
 
     def _do_some_logic(self, input_packet):
         """
         Example of a private function that does some logic.
         """
-        pass
+        if input_packet[1] == 0:
+           return self.send_ack(input_packet[0])
+        elif input_packet[1] == 1:
+            if len(self.file) != 0:
+             return self.send(self.file.pop(0))
+            else :
+                return -1
+
+    def read_file(self, file_name):
+        try:
+            file = open(file_name, "rb")
+            for chunk in iter(lambda: file.read(512), b''):
+                self.file.append(chunk)
+        except IOError:
+            print("File not accessible")
 
     def get_next_output_packet(self):
         """
@@ -94,7 +114,6 @@ class TftpProcessor(object):
         Leave this function as is.
         """
         self.last = self.packet_buffer.pop(0)
-        print("self.last = ", self.last)
         return self.last
 
     def has_pending_packets_to_be_sent(self):
@@ -116,14 +135,16 @@ class TftpProcessor(object):
         format = "!h" + str(len(file_path_on_server)) + "sB5sB"
         packing = pack(format, self.TftpPacketType.RRQ.value, bytes(file_path_on_server, "ascii"), 0,
                        bytes("octet", "ascii"), 0)
-        return packing
+        self.packet_buffer.append(packing)
 
     def upload_file(self, file_path_on_server):
-        '''2 bytes string 1 byte string 1 byte
+        '''
+        2 bytes string 1 byte string 1 byte
         --------------------------------------
         | Opcode | Filename | 0 | Mode | 0 |
         --------------------------------------
-         WRQ -> 02'''
+        WRQ -> 02
+        '''
         """
         This method is only valid if you're implementing
         a TFTP client, since the client requests or uploads
@@ -135,90 +156,75 @@ class TftpProcessor(object):
         format = "!h" + str(len(file_path_on_server)) + "sB5sB"
         packing = pack(format, self.TftpPacketType.WRQ.value, bytes(file_path_on_server, "ascii"), 0,
                        bytes("octet", "ascii"), 0)
-        return packing
-
-    def read(self, server_packet):
-        opcode = server_packet[:2]
-        unpacking = unpack("!h", opcode)
-        if unpacking[0] == 3:
-            return self.data(server_packet)
-        else:
-            self.error(server_packet)
-            return 0
-
-    def write(self, server_packet):
-        '''
-        2 bytes 2 bytes string 1 byte
-        -----------------------------------------
-        | Opcode | ErrorCode | ErrMsg | 0 |
-        -----------------------------------------
-        Figure 5-4: ERROR packet
-        '''
-        opcode = server_packet[:2]
-        unpacking = unpack("!h", opcode)
-        if unpacking[0] == 4:
-            self.ack(server_packet)
-            return 0
-        else:
-            self.error(server_packet)
-            return 1
+        self.packet_buffer.append(packing)
 
     def data(self, server_packet):
-        unpacking = unpack("!hh512s", server_packet)
-        block_number = unpacking[1]
-        data = unpacking[2]
-        print("block number = ", block_number)
-        return block_number, data
+        """
+        unpacking data, used for downloading
+        :param server_packet: received packet from server
+        :return: block number and data
+        """
+        if len(server_packet) != 516:
+            print("[Download]: Empty file")
+            exit(-1)
+        else:
+            unpacking = unpack("!hh512s", server_packet)
+            block_number = unpacking[1]
+            data_ = unpacking[2]
+            self.file.append(data_)
+            return block_number
 
     def ack(self, server_packet):
+        """
+        unpacking acknowledgement, used for uploading
+        :param server_packet: received packet from server
+        :return: block number
+        """
         unpacking = unpack("!hh", server_packet)
         block_number = unpacking[1]
-        print("block number = ", block_number)
         return block_number
 
     def error(self, server_packet):
         unpacking = unpack("!hh", server_packet[:4])
-        if unpacking[2] == 0:
+        if unpacking[1] == 0:
             print("Not defined, see error message (if any).")
-        elif unpacking[2] == 1:
+        elif unpacking[1] == 1:
             print("File not found.")
-        elif unpacking[2] == 2:
+        elif unpacking[1] == 2:
             print("Access violation.")
-        elif unpacking[2] == 3:
+        elif unpacking[1] == 3:
             print("Disk full or allocation exceeded.")
-        elif unpacking[2] == 4:
+        elif unpacking[1] == 4:
             print("Illegal TFTP operation.")
-        elif unpacking[2] == 5:
+        elif unpacking[1] == 5:
             print("Unknown transfer ID.")
-        elif unpacking[2] == 6:
+        elif unpacking[1] == 6:
             print("File already exists.")
-        elif unpacking[2] == 7:
+        elif unpacking[1] == 7:
             print("No such user.")
+        exit(-1)
 
     def send_ack(self, blocknumber):
-        print(type(blocknumber))
-        packking = pack("!hh", self.TftpPacketType.ACK.value, blocknumber)
-        return  packking
+        """
+        pack acknowledgement to send it to server while downloading
+        :param block number: block number to be sent to server while sending acknowledgement
+        """
+        packing = pack("!hh", self.TftpPacketType.ACK.value, blocknumber)
+        return packing
 
     def send(self, data):
-        return self.parse(data)
-
-    def parse(self, data):
         """
-        2 bytes 2 bytes n bytes
-        ----------------------------------
-        | Opcode | Block # | Data |
-        ----------------------------------
-        Figure 5-2: DATA packet
-
-        :param data:
-        :return:
-        """
+               2 bytes 2 bytes n bytes
+               ----------------------------------
+               | Opcode | Block # | Data |
+               ----------------------------------
+               Figure 5-2: DATA packet
+               :param data: packing data packet to be sent to server while uploading
+               """
         format = "!hh512s"
         packing = pack(format, self.TftpPacketType.DATA.value, self.blocknumber, data)
-        #print("block number = ", self.blocknumber)
         self.blocknumber = self.blocknumber + 1
-        self.packet_buffer.append(packing)
+        return packing
 
 
 def check_file_name():
@@ -257,70 +263,69 @@ def do_socket_logic(address, operation, client_socket, file_name):
 
 def upload(address, operation, client_socket, file_name, server_address):
     tftp = TftpProcessor()
-    wrq = tftp.upload_file(file_name)
-    client_socket.sendto(wrq, server_address)
+    tftp.read_file(file_name)
+    tftp.upload_file(file_name)
+    print("[Upload] sending write request")
+    client_socket.sendto(tftp.get_next_output_packet(), server_address)
     (server_packet, (add, p)) = client_socket.recvfrom(516)
-    print(server_packet)
-    print("[CLIENT] IN", server_packet, p)
-    print(add[1])
+    ret = tftp.process_udp_packet(server_packet, p)
+    print("[Upload] ACK: ", server_packet)
+    print("[Upload] address", add)
     port_add = (address, p)
-    print("[CLIENT] IN", server_packet)
-    print(add[1])
-    uploading = tftp.write(server_packet)
-    print(uploading)
-    if uploading == 0:
-        try:
-            file = open(file_name, "rb")
-            for chunk in iter(lambda: file.read(512), b''):
-                tftp.send(chunk)
-            while tftp.has_pending_packets_to_be_sent():
-                client_socket.sendto(tftp.get_next_output_packet(), port_add)
+    print("[Upload] sending data ...")
+    if ret[1] == 1:
+        while tftp.has_pending_packets_to_be_sent():
+            client_socket.sendto(tftp.get_next_output_packet(), port_add)
+            try:
+                (packet, address) = client_socket.recvfrom(516)
+                print("[Upload] Ack: ", packet)
+                tftp.process_udp_packet(packet, port_add)
+            except socket.error:
+                client_socket.sendto(tftp.last, port_add)
                 try:
-                    (packet, (address, port)) = client_socket.recvfrom(516)
-                    tftp.write(packet)
-
-                except:
-                    client_socket.sendto(tftp.last, port_add)
-                    try:
-                        (packet, (address, port)) = client_socket.recvfrom(516)
-                        tftp.write(packet)
-                    except:
-                        print("Timeout")
-        except IOError:
-            print("File not accessible")
-            pass
-
+                    (packet, address) = client_socket.recvfrom(516)
+                    print("[Upload] Ack: ", packet)
+                    tftp.process_udp_packet(packet, port_add)
+                except socket.error:
+                    print("[Upload] timeout")
+                    exit(-1)
     else:
         exit(-1)
     tftp.blocknumber = 1
-    print("last", tftp.blocknumber)
+    print("[Upload] file uploaded successfully")
 
 
 def download(address, operation, client_socket, file_name, server_address):
     tftp = TftpProcessor()
-    rrq = tftp.request_file(file_name)
-    client_socket.sendto(rrq, server_address)
+    tftp.request_file(file_name)
+    ack = tftp.get_next_output_packet()
+    client_socket.sendto(ack, server_address)
     (server_packet, add) = client_socket.recvfrom(516)
-    print(server_packet)
-    print(len(server_packet))
-    print("[CLIENT] IN", server_packet)
-    print("[CLIENT] IN", server_packet)
-    uploading = tftp.read(server_packet)
-    file = open("demofile2.txt", "ab")
-    file.write(uploading[1])
-    packed = tftp.send_ack(uploading[0])
+    print("[Download] data:", server_packet)
+    print("[Download] address", add)
+    uploading = tftp.process_udp_packet(server_packet, add)
+    file = open("test152.txt", "ab")
+    file.truncate(0)
+    if len(tftp.file) != 0:
+        file.write(tftp.file.pop(0))
+    else:
+        print("[Download]: No data")
+    print("[Download] sending ACK with block number", ack)
     if uploading[0] > 0:
         while 1:
-            client_socket.sendto(packed, add)
-            packet, address = client_socket.recvfrom(516)
-            print(packet)
-            if len(packet) < 516:
-                break
-            uploading = tftp.read(packet)
-            file.write(uploading[1])
-            packed = tftp.send_ack(uploading[0])
-
-
+            if tftp.has_pending_packets_to_be_sent():
+                client_socket.sendto(tftp.get_next_output_packet(), add)
+                packet, address = client_socket.recvfrom(516)
+                if len(packet) < 516:
+                    print("[Download] finished.")
+                    break
+                print("Downloading data ...")
+                uploading = tftp.process_udp_packet(packet, add)
+                if len(tftp.file) != 0:
+                    file.write(tftp.file.pop(0))
+                else:
+                    print("[Download]: No data")
+                print("[Download] sending ACK with block number ", uploading[0])
 
 
 def parse_user_input(address, operation, file_name=None):
@@ -330,14 +335,13 @@ def parse_user_input(address, operation, file_name=None):
     # But don't add socket code in the TftpProcessor class.
     # Feel free to delete this code as long as the
     # functionality is preserved.
-    print(operation)
     client_socket = setup_sockets(address)
     if operation == "push":
         print(f"Attempting to upload [{file_name}]...")
-        do_socket_logic(address, operation, client_socket, file_name)
     elif operation == "pull":
         print(f"Attempting to download [{file_name}]...")
-        do_socket_logic(address, operation, client_socket, file_name)
+
+    do_socket_logic(address, operation, client_socket, file_name)
 
 
 def get_arg(param_index, default=None):
@@ -376,7 +380,7 @@ def main():
     # are provided. Feel free to modify them.
     ip_address = get_arg(1, "127.0.0.1")
     operation = get_arg(2, "pull")
-    file_name = get_arg(3, "test.txt")
+    file_name = get_arg(3, "test12.txt")
 
     # Modify this as needed.
     parse_user_input(ip_address, operation, file_name)
